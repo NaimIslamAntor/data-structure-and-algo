26f04aeb8096464ea043eea9663572514aa4aaf6
Naim <naimfn249@gmail.com>
2022-10-17 15:00:11 +0600

    solving a pangram sentence

---
+++ pangram.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} sentence
+ * @return {boolean}
+ */
+ var checkIfPangram = function(sentence) {
+
+    const alphabets = 'abcdefghijklmnopqrstuvwxyz';
+
+    const alphaArr = alphabets.split('')
+
+     for(char of sentence){
+
+        const indexOfChar = alphaArr.indexOf(char)
+       
+        if (indexOfChar !== -1) {
+            alphaArr.splice(indexOfChar, 1)
+        }
+     }
+
+     return !alphaArr.length
+    
+};
+
+
+
+console.log(checkIfPangram("thequickbrownfoxjumpsoverthelazydog"));
